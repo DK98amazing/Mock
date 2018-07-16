7a65cc345c026d35963fb978bc0746cd0a6ad027
DK98amazing <gyli@utstar.com>
2018-07-16 22:26:58 +0800

    mock test

---
+++ src/main/java/Test1.java
@@ -0,0 +1,11 @@
+public class Test1 {
+    private String namepub;
+
+    public String getName() {
+        return namepub;
+    }
+
+    public void setName(String name) {
+        this.namepub = name;
+    }
+}
---
+++ src/main/java/Test2.java
@@ -0,0 +1,11 @@
+public final class Test2 extends Test1{
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
