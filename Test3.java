b1adb42de04575021691c3049e3293a2d31b37ea
DK98amazing <37374564+DK98amazing@users.noreply.github.com>
2018-07-16 22:05:47 +0800

    Add files via upload

---
+++ Test3.java
@@ -0,0 +1,11 @@
+public class Test3 {
+    private static String str;
+
+    public static String getStr() {
+        return str;
+    }
+
+    public static void setStr(String str1) {
+        str = str1;
+    }
+}
