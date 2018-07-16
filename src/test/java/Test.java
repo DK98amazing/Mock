13f20bf5dbc5a44bb7666cbf37f809ac0f809275
DK98amazing <gyli@utstar.com>
2018-07-17 00:13:43 +0800

    Test

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,3 +1,5 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
@@ -13,22 +15,30 @@ import static org.mockito.Mockito.when;
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({Test3.class})
 public class Test {
+    Test1 test1;
+    Test2 test2;
+
+    @Before
+    public void runB() {
+        test1 = Mockito.mock(Test1.class);
+        test2 = Mockito.mock(Test2.class);
+    }
+
     @org.junit.Test
     public void runA() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-
-        Test1 test1 = Mockito.mock(Test1.class);
         test1.setName("sss");
         when(test1.getName()).thenReturn("success");
         verify(test1).setName("sss");
         System.out.println(test1.getName());
 
-        Test2 test2 = Mockito.mock(Test2.class);
         when(test2.getName()).thenReturn("success");
         System.out.println(test2.getName());
 
         PowerMockito.mockStatic(Test3.class);
         when(Test3.getStr()).thenReturn("success");
         System.out.println(Test3.getStr());
-
     }
+
+    @After
+    public void runC() {}
 }
